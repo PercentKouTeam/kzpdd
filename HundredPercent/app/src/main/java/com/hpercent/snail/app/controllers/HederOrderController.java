c68f3e4547e51af2f98a1f3e300a49fe41915ed3
snailness <koude_jian@126.com>
2014-10-26 23:51:59 +0800

    add function of umeng socialize

--- HundredPercent/app/src/main/java/com/hpercent/snail/app/controllers/HederOrderController.java
+++ HundredPercent/app/src/main/java/com/hpercent/snail/app/controllers/HederOrderController.java
@@ -13,6 +13,9 @@ import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 
 import com.hpercent.snail.app.R;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.media.UMImage;
 
 /**
  * Created by koudejian on 14-8-30.
@@ -33,8 +36,9 @@ public class HederOrderController extends BaseController {
         mHeader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                test();
                 //弹出类型窗口
-                showPopupWindow();
+//                showPopupWindow();
             }
         });
     }
@@ -71,5 +75,37 @@ public class HederOrderController extends BaseController {
         params.gravity = Gravity.TOP | Gravity.RIGHT;
         params.y += mContext.getResources().getDimensionPixelSize(R.dimen.header_height);
         dialog.getWindow().setAttributes(params);
+    }
+
+    public void test(){
+        // 首先在您的Activity中添加如下成员变量
+        final UMSocialService mController = UMServiceFactory.getUMSocialService("com.umeng.share");
+        // 设置分享内容
+        mController.setShareContent("友盟社会化组件（SDK）让移动应用快速整合社交分享功能，http://www.umeng.com/social");
+        // 设置分享图片, 参数2为图片的url地址
+        mController.setShareMedia(new UMImage(mContext,
+                "http://www.umeng.com/images/pic/banner_module_social.png"));
+        // 设置分享图片，参数2为本地图片的资源引用
+        //mController.setShareMedia(new UMImage(getActivity(), R.drawable.icon));
+        // 设置分享图片，参数2为本地图片的路径(绝对路径)
+        //mController.setShareMedia(new UMImage(getActivity(),
+        //                                BitmapFactory.decodeFile("/mnt/sdcard/icon.png")));
+
+        // 设置分享音乐
+        //UMusic uMusic = new UMusic("http://sns.whalecloud.com/test_music.mp3");
+        //uMusic.setAuthor("GuGu");
+        //uMusic.setTitle("天籁之音");
+        // 设置音乐缩略图
+        //uMusic.setThumb("http://www.umeng.com/images/pic/banner_module_social.png");
+        //mController.setShareMedia(uMusic);
+
+        // 设置分享视频
+        //UMVideo umVideo = new UMVideo(
+        //          "http://v.youku.com/v_show/id_XNTE5ODAwMDM2.html?f=19001023");
+        // 设置视频缩略图
+        //umVideo.setThumb("http://www.umeng.com/images/pic/banner_module_social.png");
+        //umVideo.setTitle("友盟社会化分享!");
+        //mController.setShareMedia(umVideo);
+        mController.openShare((android.app.Activity) mContext, false);
     }
 }
